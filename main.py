a7978856675ad16f54b2dc2a563967eca7922e80
arshedgithub <arshedahmedmail@gmail.com>
2025-02-13 01:26:55 +0530

    adding emotion detection library

--- main.py
+++ main.py
@@ -1,9 +1,12 @@
 import cv2
+from facial_emotion_recognition import EmotionRecognition
 
+er = EmotionRecognition(device='cpu')
 cam = cv2.VideoCapture(0)
 
 while True:
     success, frame = cam.read()
+    frame = er.recognise_emotion(frame, return_type='BGR')
     cv2.imshow("Emotion Recognition", frame)
     key = cv2.waitKey(1)
     if key == 27:
